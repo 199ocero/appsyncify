d323c33fa762878fa3e43260da6d342337e58668
199ocero <199ocero@gmail.com>
2023-10-01 21:21:14 +0800

    added constant for app names and codes

--- app/Enums/Constant.php
+++ app/Enums/Constant.php
@@ -4,7 +4,25 @@ namespace App\Enums;
 
 class Constant
 {
-    // status
+    /**
+     * Status of apps
+     */
     const ACTIVE = 1;
     const INACTIVE = 0;
+
+    /**
+     * App names
+     */
+    const SALESFORCE = 'Salesforce';
+    const MAILCHIMP = 'Mailchimp';
+    // others apps here...
+
+    /**
+     * App code
+     */
+    const APP_CODE = [
+        self::SALESFORCE => 'salesforce',
+        self::MAILCHIMP => 'mailchimp',
+        // others apps code here...
+    ];
 }
