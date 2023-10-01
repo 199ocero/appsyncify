51b5ffde83995488b9971cff09091643df2088eb
199ocero <199ocero@gmail.com>
2023-10-01 16:13:42 +0800

    add app combination model

---
+++ app/Models/AppCombination.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppCombination extends Model
+{
+    use HasFactory;
+
+    protected $table = 'app_combination';
+
+    protected $fillable = [
+        'first_app_id',
+        'second_app_id',
+    ];
+
+    public function firstApp()
+    {
+        return $this->belongsTo(App::class, 'first_app_id', 'id');
+    }
+
+    public function secondApp()
+    {
+        return $this->belongsTo(App::class, 'second_app_id', 'id');
+    }
+}
