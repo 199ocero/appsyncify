d1e5712073387a205fd407414b60457674416703
199ocero <199ocero@gmail.com>
2023-10-08 07:00:15 +0800

    added mailchimp oauth

--- app/Forms/WizardStep/MailchimpWizardStep.php
+++ app/Forms/WizardStep/MailchimpWizardStep.php
@@ -2,8 +2,11 @@
 
 namespace App\Forms\WizardStep;
 
-use App\Forms\Contracts\HasWizardStep;
 use Filament\Forms;
+use App\Models\Token;
+use App\Enums\Constant;
+use App\Models\Integration;
+use App\Forms\Contracts\HasWizardStep;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\Component;
 use Filament\Notifications\Notification;
@@ -32,10 +35,35 @@ class MailchimpWizardStep implements HasWizardStep
             ->schema([
                 Forms\Components\Actions::make([
                     Forms\Components\Actions\Action::make($app->app_code)
-                        ->label('Connect to ' . $app->name)
-                        ->url(route('auth.' . $app->app_code))
-                        ->icon('heroicon-o-bolt')
-                ])
+                        ->label(fn () => $token_id ? 'Connected to ' . $app->name : 'Connect to ' . $app->name)
+                        ->url(function () use ($app, $integration_id, $type) {
+                            session([
+                                'mailchimp_app_id' => $app->id,
+                                'integration_id' => $integration_id,
+                                'type' => $type
+                            ]);
+                            return route('auth.' . $app->app_code);
+                        })
+                        ->icon(fn () => $token_id ? 'heroicon-o-check-badge' : 'heroicon-o-bolt')
+                        ->color(fn () => $token_id ? 'gray' : 'primary')
+                        ->disabled(fn () => $token_id ? true : false),
+                    Forms\Components\Actions\Action::make('disconnect_' . $app->app_code)
+                        ->label('Disconnect')
+                        ->icon('heroicon-o-bolt-slash')
+                        ->color('primary')
+                        ->requiresConfirmation()
+                        ->modalHeading('Disconnect from ' . $app->name)
+                        ->modalSubmitActionLabel('Yes, disconnect')
+                        ->modalIcon('heroicon-o-bolt-slash')
+                        ->action(function () use ($integration_id, $token_id, $type) {
+                            $updateDataKey = $type == Constant::FIRST_APP ? 'first_app' : 'second_app';
+                            Integration::query()->find($integration_id)->update([
+                                "{$updateDataKey}_settings" => null,
+                            ]);
+                            Token::query()->find($token_id)->delete();
+                        })
+                        ->hidden($token_id ? false : true)
+                ]),
             ]);
     }
 }
--- app/Http/Controllers/MailchimpOAuthController.php
+++ app/Http/Controllers/MailchimpOAuthController.php
@@ -14,12 +14,47 @@ class MailchimpOAuthController extends Controller
 {
     public function redirectToMailchimp()
     {
-        return Socialite::driver('mailchimp')->redirect();
+        if (session()->has(['mailchimp_app_id', 'integration_id', 'type'])) {
+            return Socialite::driver('mailchimp')->redirect();
+        }
+
+        abort(404);
     }
 
     public function handleMailchimpCallback()
     {
-        $user = Socialite::driver('mailchimp')->user();
-        dd($user);
+        if (session()->has(['mailchimp_app_id', 'integration_id', 'type'])) {
+            $user = Socialite::driver('mailchimp')->user();
+
+            $token = Token::updateOrCreate(
+                [
+                    'user_id' => auth()->user()->id,
+                    'app_id' => session('mailchimp_app_id'),
+                ],
+                [
+                    'token' => Crypt::encryptString($user->token),
+                ]
+            );
+
+            if (session('type') == Constant::FIRST_APP || session('type') == Constant::SECOND_APP) {
+                $updateDataKey = session('type') == Constant::FIRST_APP ? 'first_app' : 'second_app';
+
+                Integration::query()->find(session('integration_id'))->update([
+                    "{$updateDataKey}_token_id" => $token->id
+                ]);
+            }
+
+            $integration_id = session('integration_id');
+
+            session()->forget([
+                'mailchimp_app_id',
+                'integration_id',
+                'type'
+            ]);
+
+            return redirect()->route('filament.client.resources.integrations.setup', $integration_id);
+        }
+
+        abort(404);
     }
 }
