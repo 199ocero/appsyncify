63dd69d9f007beb2e82ac711d09e9c9d708fc1bc
199ocero <199ocero@gmail.com>
2023-10-12 20:41:47 +0800

    added custom tailwind css classes

--- resources/views/forms/components/field-mapping.blade.php
+++ resources/views/forms/components/field-mapping.blade.php
@@ -1,11 +1,21 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
-    <div class="flex items-center justify-between gap-4">
-        <x-filament::input.wrapper class="p-3 w-full">
-            <span class="text-sm">{{ $field_1 }}</span>
-        </x-filament::input.wrapper>
-        <x-filament::icon-button icon="heroicon-o-chevron-double-right" size="sm" disabled="true" />
-        <x-filament::input.wrapper class="p-3 w-full">
-            <span class="text-sm">{{ $field_2 }}</span>
-        </x-filament::input.wrapper>
-    </div>
+    @for ($i = 0; $i < $mappedItems['COUNT']; $i++)
+        <div class="field-mapping-item">
+            <div class="field-mapping-app">
+                <img class="field-mapping-app-logo" src="{{ asset($mappedItems['FIRST_APP_LOGO']) }}"
+                    alt="Salesforce Logo">
+                <x-filament::input.wrapper class="py-2 px-4 w-full">
+                    <span class="text-sm">{{ $mappedItems['FIRST_APP'][$i] }}</span>
+                </x-filament::input.wrapper>
+            </div>
+            <x-filament::icon-button icon="heroicon-o-chevron-double-right" size="sm" disabled="true" />
+            <div class="field-mapping-app">
+                <x-filament::input.wrapper class="py-2 px-4 w-full">
+                    <span class="text-sm">{{ $mappedItems['SECOND_APP'][$i] }}</span>
+                </x-filament::input.wrapper>
+                <img class="field-mapping-app-logo" src="{{ asset($mappedItems['SECOND_APP_LOGO']) }}"
+                    alt="Mailchimp Logo">
+            </div>
+        </div>
+    @endfor
 </x-dynamic-component>
