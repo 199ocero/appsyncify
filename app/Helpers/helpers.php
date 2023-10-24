c050d148a8a87db88572c423c54aa70d30f6abed
199ocero <199ocero@gmail.com>
2023-10-24 19:25:40 +0800

    added helper

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Enums\Constant;
+use App\Models\Integration;
+
+if (!function_exists('getTokenId')) {
+    function getTokenId(string $type, Integration $integration): int
+    {
+        if ($type == Constant::FIRST_APP) {
+            return $integration->first_app_token_id;
+        }
+
+        return $integration->second_app_token_id;
+    }
+}
+
+if (!function_exists('getSettings')) {
+    function getSettings(string $type, Integration $integration): array
+    {
+        if ($type == Constant::FIRST_APP) {
+            return json_decode($integration->first_app_settings, true);
+        }
+
+        return json_decode($integration->second_app_settings, true);
+    }
+}
