b8af93b9646faf57c06794300d028db01dd19d72
199ocero <199ocero@gmail.com>
2023-10-30 13:57:33 +0800

    added actions

--- app/Filament/Client/Resources/IntegrationResource/Pages/LaunchIntegration.php
+++ app/Filament/Client/Resources/IntegrationResource/Pages/LaunchIntegration.php
@@ -2,18 +2,19 @@
 
 namespace App\Filament\Client\Resources\IntegrationResource\Pages;
 
-use App\Enums\Constant;
-use Filament\Tables\Table;
 use Filament\Tables;
+use App\Enums\Constant;
+use App\Models\SyncLog;
+use App\Models\Operation;
+use Filament\Tables\Table;
+use App\Models\Integration;
 use Filament\Resources\Pages\Page;
+use Filament\Support\Colors\Color;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Tables\Concerns\InteractsWithTable;
 use App\Filament\Client\Resources\IntegrationResource;
-use App\Models\Operation;
-use App\Models\SyncLog;
-use Filament\Support\Colors\Color;
 
 class LaunchIntegration extends Page implements HasForms, HasTable
 {
@@ -23,12 +24,57 @@ class LaunchIntegration extends Page implements HasForms, HasTable
 
     protected static string $view = 'filament.client.resources.integration-resource.pages.launch-integration';
 
+    public Integration $integration;
+
+    protected $schedule;
+
+    public function mount($integration)
+    {
+        if ($integration) {
+            if ($integration->is_finished) {
+                $integration->load([
+                    'appCombination.firstApp',
+                    'appCombination.secondApp',
+                    'firstAppToken',
+                    'secondAppToken'
+                ]);
+                $this->integration = $integration;
+            } else {
+                abort(403);
+            }
+        } else {
+            abort(404);
+        }
+    }
+
+    public function boot()
+    {
+        $this->heading = $this->integration->name;
+        $this->subheading = $this->integration->description;
+        $this->schedule = $this->integration->schedule ? json_decode($this->integration->schedule, true) : null;
+    }
+
     public function table(Table $table): Table
     {
-        $operation = Operation::query()->where('actor_id', auth()->user()->id)->where('status', Constant::STATUS_PENDING)->first();
+        $operation = Operation::query()->where('actor_id', auth()->user()->id)->where('status', Constant::STATUS_RUNNING)->first();
+
+        $actions = [];
+
+        if (!$this->schedule) {
+            $actions = [
+                Tables\Actions\Action::make('manual_sync')
+                    ->label('Sync Now')
+                    ->icon('heroicon-o-arrow-path')
+                    ->requiresConfirmation()
+                    ->modalHeading('Sync Now')
+                    ->modalDescription('This will create a new sync operation.')
+                    ->modalSubmitActionLabel('Yes, sync now')
+                    ->modalIcon('heroicon-o-arrow-path'),
+            ];
+        }
 
         return $table
-            ->query(SyncLog::query()->where('operation_id', $operation->id))
+            ->query(SyncLog::query()->where('operation_id', $operation ? $operation->id : null))
             ->columns([
                 Tables\Columns\TextColumn::make('operation_id')
                     ->label('Operation ID')
@@ -76,6 +122,15 @@ class LaunchIntegration extends Page implements HasForms, HasTable
             ])
             ->bulkActions([
                 // ...
-            ]);
+            ])
+            ->emptyStateIcon('heroicon-o-rocket-launch')
+            ->emptyStateHeading('No sync running.')
+            ->emptyStateDescription(function () {
+                if (!$this->schedule) {
+                    return "You've chosen manual syncing. You can initiate the sync now.";
+                }
+                return 'Your scheduled sync has not started yet.';
+            })
+            ->emptyStateActions($actions);
     }
 }
