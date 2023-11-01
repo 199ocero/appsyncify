7fc2cceeb6294ee1e33ab49a126b99502508d68c
199ocero <199ocero@gmail.com>
2023-11-01 20:55:09 +0800

    added login test

---
+++ tests/Feature/Admin/LoginTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\AdminUser;
+use function Pest\Livewire\livewire;
+use function Pest\Laravel\{actingAs, get, post};
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->adminUser = AdminUser::factory()->create();
+});
+
+it('can render the admin login page', function () {
+    get(route('filament.admin.auth.login'))->assertOk()->assertSuccessful();
+});
+
+it('can login as an admin', function () {
+    livewire(\Filament\Pages\Auth\Login::class)
+        ->fillForm([
+            'email' => $this->adminUser->email,
+            'password' => 'password',
+        ])
+        ->call('authenticate')
+        ->assertHasNoFormErrors();
+});
+
+it('can validate the admin login', function () {
+    livewire(\Filament\Pages\Auth\Login::class)
+        ->fillForm([
+            'email' => null,
+            'password' => null,
+        ])
+        ->call('authenticate')
+        ->assertHasFormErrors([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+});
