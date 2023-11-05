89d4201a4a715d28bd8dedbcb59f01f8c52655d5
199ocero <199ocero@gmail.com>
2023-11-05 16:43:00 +0800

    added new function for interface

--- app/Services/Contracts/HasSynchronizer.php
+++ app/Services/Contracts/HasSynchronizer.php
@@ -8,6 +8,8 @@ use App\Models\Integration;
 interface HasSynchronizer
 {
     public function getIntegration(Integration $integration): Integration;
+    public function getFirstAppData(): array;
+    public function getSecondAppData(): array;
     public function getFields(array $defaultFields = []): array;
     public function syncData(): void;
 }
