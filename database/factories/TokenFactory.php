8dd9b680374b6690156af353d8a7dcf71a09f44d
199ocero <199ocero@gmail.com>
2023-11-05 21:04:26 +0800

    added token factory

---
+++ database/factories/TokenFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Token>
+ */
+class TokenFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'app_id' => 1,
+            'user_id' => 1,
+            'token' => Str::random(30),
+            'refresh_token' => null
+        ];
+    }
+}
